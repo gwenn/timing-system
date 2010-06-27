fff9b5ee936d3b35963647a8bc1f0681ad207329
gwenn <gtreguier@gmail.com>
2010-06-27 14:29:22 +0200

    Other attempt to fix racer progression/regression.

--- race_schema.sql
+++ race_schema.sql
@@ -54,6 +54,8 @@ CREATE TABLE result (
     nb          INTEGER NOT NULL CHECK (nb >= 0), -- Number of manifests
     duration    INTEGER NOT NULL CHECK (duration >= 0), -- Number of seconds
     rank        INTEGER NOT NULL CHECK (rank >= 1),
+    prevRank    INTEGER CHECK (prevRank IS NULL OR prevRank >= 1), -- Previous rank
+    latency     INTEGER CHECK (latency IS NULL OR latency >= 0), -- Previous rank latency
     PRIMARY KEY (type, raceId, racerId),
     FOREIGN KEY (raceId) REFERENCES race(id), -- ON DELETE CASCADE
     FOREIGN KEY (racerId) REFERENCES racer(id) -- ON DELETE CASCADE
@@ -65,14 +67,31 @@ CREATE TABLE resultHisto (
     raceId      INTEGER NOT NULL,
     racerId     INTEGER NOT NULL,
     rank        INTEGER NOT NULL CHECK (rank >= 1),
+    prevRank    INTEGER CHECK (prevRank IS NULL OR prevRank >= 1), -- Previous rank
+    latency     INTEGER CHECK (latency IS NULL OR latency >= 0), -- Previous rank latency
     PRIMARY KEY (type, raceId, racerId),
     FOREIGN KEY (raceId) REFERENCES race(id), -- ON DELETE CASCADE
     FOREIGN KEY (racerId) REFERENCES racer(id) -- ON DELETE CASCADE
 );
 
-CREATE TRIGGER rank_progression AFTER DELETE ON result
+CREATE TRIGGER result_backup AFTER DELETE ON result
+WHEN (SELECT 1 FROM race r WHERE r.id = OLD.raceId AND r.status = 1) IS NULL
 BEGIN
-    REPLACE INTO resultHisto VALUES (OLD.type, OLD.raceId, OLD.racerId, OLD.rank);
+    REPLACE INTO resultHisto VALUES (OLD.type, OLD.raceId, OLD.racerId, OLD.rank, OLD.prevRank, OLD.latency);
+END;
+
+CREATE TRIGGER rank_progression AFTER INSERT ON result
+WHEN (SELECT 1 FROM race r WHERE r.id = NEW.raceId AND r.status = 1) IS NULL
+BEGIN
+    -- TODO Adjust latency...
+    UPDATE result SET
+    prevRank =
+        (SELECT CASE WHEN (OLD.rank <> NEW.rank OR OLD.latency > 10) THEN OLD.rank ELSE OLD.prevRank END
+            FROM resultHisto OLD WHERE OLD.type = NEW.type AND OLD.raceId = NEW.raceId AND OLD.racerId = NEW.racerId),
+    latency = 
+        (SELECT CASE WHEN (OLD.rank <> NEW.rank OR OLD.latency > 10) THEN 0 ELSE OLD.latency + 1 END
+            FROM resultHisto OLD WHERE OLD.type = NEW.type AND OLD.raceId = NEW.raceId AND OLD.racerId = NEW.racerId)
+    WHERE type = NEW.type AND raceId = NEW.raceId AND racerId = NEW.racerId;
 END;
 
 CREATE TRIGGER closed_race_lock1 AFTER INSERT ON timelog
@@ -105,54 +124,59 @@ BEGIN
         WHERE raceId = NEW.raceId GROUP BY racerId;
     -- Generate results
     ---- Overall results
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 0, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
 	        WHERE p2.raceId = p1.raceId
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         WHERE raceId = NEW.raceId;
     ---- Results by country
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 1, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.country = r1.country
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = NEW.raceId;
     ---- Results by gender
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 2, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.gender = r1.gender
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = NEW.raceId;
     ---- Results by country and gender
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 3, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.country = r1.country
             AND r2.gender = r1.gender
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = NEW.raceId;
     -- Update last time results generation to now
     REPLACE INTO resultVersion VALUES (NEW.raceId, strftime('%s','now'));
+    -- Remove intermediary data/stats
+    -- DELETE FROM position WHERE raceId = NEW.raceId;
 END;
 
+-- To regenerate result (after manual corrections in timelog for example), just
+-- change race status: UPDATE race set status = 1 where id = ?
 CREATE TRIGGER race_end AFTER UPDATE OF status ON race
 WHEN NEW.status = 1
 BEGIN
+    DELETE FROM resultHisto WHERE raceId = OLD.id; -- TODO Validate
 -- TODO Keep synchronized with results_generation
     -- Create intermediary data/stats
     DELETE FROM position WHERE raceId = OLD.id;
@@ -163,49 +187,51 @@ BEGIN
         WHERE raceId = OLD.id GROUP BY racerId;
     -- Generate results
     ---- Overall results
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 0, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
 	        WHERE p2.raceId = p1.raceId
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         WHERE raceId = OLD.id;
     ---- Results by country
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 1, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.country = r1.country
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = OLD.id;
     ---- Results by gender
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 2, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.gender = r1.gender
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = OLD.id;
     ---- Results by country and gender
-    REPLACE INTO result (type, raceId, racerId, nb, duration, rank)
+    REPLACE INTO result
         SELECT 3, raceId, racerId, nb, duration, (SELECT count(*)
 	        FROM position p2
             INNER JOIN racer r2 ON r2.id = p2.racerId
 	        WHERE p2.raceId = p1.raceId
             AND r2.country = r1.country
             AND r2.gender = r1.gender
-	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration)
+	        AND p2.nb >= p1.nb AND p2.duration <= p1.duration), NULL, NULL
         FROM position p1
         INNER JOIN racer r1 ON r1.id = p1.racerId
         WHERE raceId = OLD.id;
     -- Update last time results generation to now
     REPLACE INTO resultVersion VALUES (OLD.id, strftime('%s','now'));
+    -- Remove intermediary data/stats
+    -- DELETE FROM position WHERE raceId = OLD.id;
 END;
 
 /*
--- race_test_queries.sql
+++ race_test_queries.sql
@@ -1,44 +1,40 @@
 PRAGMA recursive_triggers=ON;
 UPDATE race SET status = 1 WHERE id = 1; -- trigger results generation
 SELECT 'Overall results';
-SELECT result.rank, CASE WHEN resultHisto.rank IS NULL THEN NULL
-                         WHEN result.rank - resultHisto.rank > 0 THEN '+'
-                         WHEN result.rank - resultHisto.rank < 0 THEN '-'
+SELECT rank, CASE WHEN prevRank IS NULL THEN NULL
+                         WHEN rank - prevRank > 0 THEN '+'
+                         WHEN rank - prevRank < 0 THEN '-'
                          ELSE '=' END as delta, number, firstName, company, city, duration, country
 FROM result
 INNER JOIN racer ON racer.id = result.racerId
-LEFT OUTER JOIN resultHisto ON resultHisto.type = result.type AND resultHisto.raceId = result.raceId AND resultHisto.racerId = result.racerId
 WHERE result.raceId = 1 AND result.type = 0
-ORDER BY result.rank;
+ORDER BY rank;
 SELECT 'Results by country';
-SELECT result.rank, CASE WHEN resultHisto.rank IS NULL THEN NULL
-                         WHEN result.rank - resultHisto.rank > 0 THEN '+'
-                         WHEN result.rank - resultHisto.rank < 0 THEN '-'
+SELECT rank, CASE WHEN prevRank IS NULL THEN NULL
+                         WHEN rank - prevRank > 0 THEN '+'
+                         WHEN rank - prevRank < 0 THEN '-'
                          ELSE '=' END as delta, number, firstName, company, city, duration, country
 FROM result
 INNER JOIN racer ON racer.id = result.racerId
-LEFT OUTER JOIN resultHisto ON resultHisto.type = result.type AND resultHisto.raceId = result.raceId AND resultHisto.racerId = result.racerId
 WHERE result.raceId = 1 AND result.type = 1
-ORDER BY country, result.rank;
+ORDER BY country, rank;
 SELECT 'Results by gender';
-SELECT result.rank, CASE WHEN resultHisto.rank IS NULL THEN NULL
-                         WHEN result.rank - resultHisto.rank > 0 THEN '+'
-                         WHEN result.rank - resultHisto.rank < 0 THEN '-'
+SELECT rank, CASE WHEN prevRank IS NULL THEN NULL
+                         WHEN rank - prevRank > 0 THEN '+'
+                         WHEN rank - prevRank < 0 THEN '-'
                          ELSE '=' END as delta, number, firstName, company, city, duration, country,
        CASE gender WHEN 1 THEN 'M' ELSE 'F' END AS g
 FROM result
 INNER JOIN racer ON racer.id = result.racerId
-LEFT OUTER JOIN resultHisto ON resultHisto.type = result.type AND resultHisto.raceId = result.raceId AND resultHisto.racerId = result.racerId
 WHERE result.raceId = 1 AND result.type = 2
-ORDER BY g, result.rank;
+ORDER BY g, rank;
 SELECT 'Results by country and gender';
-SELECT result.rank, CASE WHEN resultHisto.rank IS NULL THEN NULL
-                         WHEN result.rank - resultHisto.rank > 0 THEN '+'
-                         WHEN result.rank - resultHisto.rank < 0 THEN '-'
+SELECT rank, CASE WHEN prevRank IS NULL THEN NULL
+                         WHEN rank - prevRank > 0 THEN '+'
+                         WHEN rank - prevRank < 0 THEN '-'
                          ELSE '=' END as delta, number, firstName, company, city, duration, country,
        CASE gender WHEN 1 THEN 'M' ELSE 'F' END AS g
 FROM result
 INNER JOIN racer ON racer.id = result.racerId
-LEFT OUTER JOIN resultHisto ON resultHisto.type = result.type AND resultHisto.raceId = result.raceId AND resultHisto.racerId = result.racerId
 WHERE result.raceId = 1 AND result.type = 3
-ORDER BY country, g, result.rank;
+ORDER BY country, g, rank;
